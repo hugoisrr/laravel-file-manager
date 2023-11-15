a0ab509bba03be37156870c14bdca2b6b67cf39e
Hugo Ramirez <hugo.ramirez@fruitcore.de>
2023-11-15 17:42:43 +0100

    - initial commit - DB Schema created - migrations created - Navigation created

---
+++ resources/js/Components/app/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo
+                    class="block w-auto text-gray-800 fill-current h-9"
+                />
+            </Link>
+            LaraStorage
+        </div>
+        <div class="px-3">
+            <CreateNewDropdown />
+            <div class="py-3">
+                <NavLink href="/">My Files</NavLink>
+                <NavLink href="/" :active="true">Share with me</NavLink>
+                <NavLink href="/">Shared by me</NavLink>
+                <NavLink href="/">Trash</NavLink>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import NavLink from "@/Components/NavLink.vue";
+import CreateNewDropdown from "@/Components/app/CreateNewDropdown.vue";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<style scoped></style>
